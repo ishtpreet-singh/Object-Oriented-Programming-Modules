db4b570ca0d8ab01ef5f049c77c607860c814793
Ishtpreet Singh <aulakhishtsingh21@gmail.com>
2025-08-25 15:32:49 +0300

    Add files via upload

---
+++ M1_4Task_3.java
@@ -0,0 +1,36 @@
+import java.util.Scanner;
+
+public class M1_4Task_3 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter the size of the array: ");
+        int n = sc.nextInt();
+
+        int[] arr = new int[n];
+        
+        for (int i = 0; i < n; i++) {
+            arr[i] = sc.nextInt();
+        }
+
+        int[] unique = new int[n];
+        int uniqueCount = 0;
+        for (int i = 0; i < n; i++) {
+            boolean isDuplicate = false;
+            for (int j = 0; j < uniqueCount; j++) {
+                if (arr[i] == unique[j]) {
+                    isDuplicate = true;
+                    break;
+                }
+            }
+            if (!isDuplicate) {
+                unique[uniqueCount] = arr[i];
+                uniqueCount++;
+            }
+        }
+        System.out.println("Array after removing duplicates:");
+        for (int i = 0; i < uniqueCount; i++) {
+            System.out.print(unique[i] + " ");
+        }
+        sc.close();
+    }
+}
